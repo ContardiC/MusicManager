829cddede020ee5341594b5df270a2bef45ecb65
Carlo Contardi <carlocontardi@MacBook-Air-di-Carlo.local>
2024-10-10 13:10:39 +0200

    Add Album class

---
+++ Album.java
@@ -0,0 +1,78 @@
+import java.util.Date;
+
+public class Album{
+    private String title;
+    private String artist;
+    private Date releaseDate;
+    private String genre;
+    private int trackCount;
+    private int totalDuration; // in minutes
+    private String recordLabel;
+    // Constructor
+    public Album(String title, String artist, Date releaseDate, String genre, int trackCount, int totalDuration, String recordLabel) {
+        this.title = title;
+        this.artist = artist;
+        this.releaseDate = releaseDate;
+        this.genre = genre;
+        this.trackCount = trackCount;
+        this.totalDuration = totalDuration;
+        this.recordLabel = recordLabel;
+    }
+
+    // Getter and Setter methods
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getTrackCount() {
+        return trackCount;
+    }
+
+    public void setTrackCount(int trackCount) {
+        this.trackCount = trackCount;
+    }
+
+    public int getTotalDuration() {
+        return totalDuration;
+    }
+
+    public void setTotalDuration(int totalDuration) {
+        this.totalDuration = totalDuration;
+    }
+
+    public String getRecordLabel() {
+        return recordLabel;
+    }
+
+    public void setRecordLabel(String recordLabel) {
+        this.recordLabel = recordLabel;
+
+}
