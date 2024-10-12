a4121bf495ce805a9cd7fe9558b3b89c3bace9b2
ContardiC <c.contardi@spacecoding.it>
2024-10-12 11:25:01 +0200

    Update AlbumIndex class

--- AlbumIndex.java
+++ AlbumIndex.java
@@ -1,11 +1,17 @@
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 public class AlbumIndex implements Comparable<AlbumIndex>{
     String key; // Album title
-    long value; // Album position
+    int value; // Album position
+    AlbumIndex(String key, int value){
+        this.key=key;
+        this.value=value;
+    }
     @Override
     public int compareTo(AlbumIndex o) {
         return key.compareTo(o.key);
     }
-    public void createIndex(){
-        
-    }
+    
 }
